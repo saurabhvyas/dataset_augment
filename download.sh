73477d57aa062a852087b984d6c63e9ccd84d2eb
Saurabh Vyas <saurabhorange@msn.com>
2017-10-15 15:41:46 +0530

    added a fix for error with -o in youtube dl

--- download.sh
+++ download.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 #echo '$1 = ' $1
+
+title=$(youtube-dl --get-title $1)
+
 cd downloads
-youtube-dl --write-auto-sub --sub-lang en --skip-download $1
+
+youtube-dl --write-auto-sub --sub-lang en --skip-download  --output "%(title)s.%(ext)s" $1
+
+#ffmpeg -i "${title}.srt" "${title}.vtt"
+
+#sed -r '/^[0-9]+$/{N;d}' "${title}.srt" > "${title}.txt"
+
+#sed 's/^ *//; s/ *$//; /^$/d; /^\s*$/d' "${title}.txt" > "${title}_final.txt"
+
+#rm "${title}.txt"
+
+
 
 
