2ca655b628a53679d4319bfeb7a37f078770e6a8
Saurabh Vyas <saurabhorange@msn.com>
2017-11-05 19:26:57 +0530

    flux master

--- api.sh
+++ api.sh
@@ -2,4 +2,4 @@
 
 echo "$1"
 
-wget -O result.json  --header=Content-Type:application/json "https://www.googleapis.com/youtube/v3/search?q=$1&part=snippet&&maxResults=25&type=video&key=AIzaSyA0lKKbxXaOTZTuAkRLX6yThGFmLhqylI8"
+wget -O result.json  --header=Content-Type:application/json "https://www.googleapis.com/youtube/v3/search?q="{$1}computer_science"&part=snippet&&maxResults=25&type=video&key=AIzaSyA0lKKbxXaOTZTuAkRLX6yThGFmLhqylI8"
